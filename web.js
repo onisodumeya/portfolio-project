23b05cdb275577abe0718363982980e1d228b324
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-04-24 23:09:57 +0100

    feature: added the filter toggle

--- web.js
+++ web.js
@@ -18,30 +18,32 @@ checkbox.forEach((check, index) => {
     checkFill[index].classList.toggle("check-fill-block");
     checkBorder[index].classList.toggle("check-border-green");
   })
-})
-
-const closeFilterBtn = document.getElementById("closeFilterBtn");
-const filter = document.getElementById("filter")
-const filterSection = document.getElementById("filterSection")
-const filterHead = document.getElementById("filterHead")
-const filterIcon = document.getElementById("filterIcon")
-
-closeFilterBtn.addEventListener("click", () => {
-  filter.classList.remove("filter")
-  filter.classList.add("filter-closed")
-  filter.style.height = "fit-content"
-  closeFilterBtn.style.display = "none"
-  filterHead.classList.remove("filter-head");
-  filterHead.classList.add("filter-head-closed");
-  filterIcon.style.display = "block"
-})
-
-filterHead.addEventListener("click", () => {
-    filter.classList.remove("filter-closed");
-    filter.classList.add("filter");
-    closeFilterBtn.style.display = "block";
-    filterHead.classList.remove("filter-head-closed");
-    filterHead.classList.add("filter-head");
-    filterIcon.style.display = "none";
   
+})
+
+const filter = document.getElementById("filter");
+const filterSection = document.getElementById("filterSection");
+const closeFilter = document.getElementById("closeFilterBtn");
+const closeFilterDisplay = document.getElementById("closedFilterDisplay");
+const firstRow = document.getElementById("firstRow");
+const contentGrid = document.getElementById("contentGrid")
+const rightSection = document.getElementById("rightSection")
+
+closeFilter.addEventListener("click", () => {
+  filterSection.style.display = "none";
+  filter.classList.add("filter-closed")
+  filter.style.position = "absolute"
+  closeFilterDisplay.style.display = "flex";
+  firstRow.classList.add("first-row-filter-closed")
+  contentGrid.classList.add("content-grid-closed-filter");
+  contentGrid.style.alignItems = "end";
+})
+
+closeFilterDisplay.addEventListener("click", () => {
+  filterSection.style.display = "flex";
+  filter.classList.remove("filter-closed");
+  filter.style.position = "relative";
+  firstRow.classList.remove("first-row-filter-closed");
+  contentGrid.classList.remove("content-grid-closed-filter");
+  closeFilterDisplay.style.display = "none";
 })
