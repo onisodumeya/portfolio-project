5d6b644a607546111d2cd51f3292ab1c45dbaa28
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-05-25 20:58:02 +0100

    Authentication endpoints connected

---
+++ javascript/login.js
@@ -0,0 +1,43 @@
+const loginBtn = document.getElementById("loginBtn");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+
+
+
+async function loginUser() {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  try {
+    const response = await fetch(
+      "https://jodna-portfolio.onrender.com/auth/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+        
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    
+
+    if (data.message === "successful") {
+      
+      localStorage.setItem("userId", data.data.user._id);
+      // window.location.href = "./dashboard.html";
+    }else{
+      message.first
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    alert("An error occurred while trying to log in.");
+  }
+}
+
+
+loginBtn.addEventListener("click", () => {
+  loginUser();
+})
---
+++ javascript/signup.js
@@ -0,0 +1,76 @@
+const signUpBtn = document.getElementById("signUpBtn");
+const firstName = document.getElementById("firstname").value;
+const lastName = document.getElementById("lastname").value;
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+const emailMessage = document.getElementById("emailMessage");
+
+email.addEventListener("input", () => {
+  const emailValue = emailInput.value;
+  const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
+
+  if (emailInput === "") {
+    emailMessage.textContent = "";
+  } else if (!isValid) {
+    emailMessage.textContent = "Invalid email address";
+    emailMessage.style.color = "red";
+  }else{
+    emailMessage.textContent = "";
+  }
+});
+
+const passwordMessage = document.getElementById("passwordMessage");
+
+confirmPassword.addEventListener("input", () => {
+  const confirmPasswordInput = confirmPassword.value;
+  const passwordValue = password.value
+
+  if (confirmPasswordInput === passwordValue) {
+    passwordMessage.textContent = "";
+}else if(passwordValue === "" && confirmPasswordInput == ""){
+    passwordMessage.textContent = "";
+  } else{
+    passwordMessage.textContent = "Passwords do not match";
+    passwordMessage.style.color = "red";
+  }
+});
+
+async function loginUser() {
+  const firstname = document.getElementById("firstname").value.trim();
+  const lastname = document.getElementById("lastname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  try {
+    const response = await fetch(
+      "https://jodna-portfolio.onrender.com/auth/signup",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({firstname, lastname, email, password})
+      }
+    );
+
+    const data = await response.json();
+    console.log(data.data.user._id);
+    
+
+    if (response.ok) {
+      // window.location.href = "./login.html"
+      console.log("Login successful:", data);
+    } else {
+      console.error("Login failed:", data.message);
+    }
+  } catch (error) {
+    console.error("Network error:", error);
+  }
+}
+
+signUpBtn.addEventListener("click", () => {
+  loginUser();
+  
+});
